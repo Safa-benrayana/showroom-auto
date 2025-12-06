40eb7b32d66d051f5044c0ff71e48102690bdf34
safa <safabenrayana711@gmail.com>
2025-12-06 18:22:39 +0100

    first commit

---
+++ src/app/components/search-bar/search-bar.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Auto } from '../../interfaces/auto';
+import { CurrencyPipe, NgClass, NgStyle } from '@angular/common';
+
+
+@Component({
+  selector: 'app-search-bar',
+  imports: [CurrencyPipe, NgClass, NgStyle],
+  templateUrl: './search-bar.html',
+  styleUrl: './search-bar.css'
+})
+export class SearchBar {
+  @Input() autos:Auto[]=[]
+
+  @Output() onSelectAuto=new EventEmitter<Auto>()
+
+  selectedAutos:Auto[]=[]
+
+  selectAutoList(brand:string){
+    this.selectedAutos=this.autos.filter(
+      x=>x.brand.toLowerCase().startsWith(brand.toLowerCase())
+    )
+  }
+  showDetails(auto:Auto){
+    this.onSelectAuto.emit(auto)
+  }
+
+  autoTitleStyle(auto:Auto){
+    if(auto.power>=10)
+      return {'color':'#800000'}
+    else
+      return {'color': 'black'}
+  }
+
+}
